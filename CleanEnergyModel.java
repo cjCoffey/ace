9d040cd2ea37aefce815c4dc5139cf9ff1dcb40c
CJ Coffey <40609439+cjCoffey@users.noreply.github.com>
2018-10-31 18:15:06 -0400

    more changes
    probably broke it

--- CleanEnergyModel.java
+++ CleanEnergyModel.java
@@ -2,6 +2,36 @@ public class CleanEnergyModel
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello world!");
+		//args = [name] use this line to keep track of args needed
+
+		System.out.print("Hello " + args[0] + "!");
+		//parse weather file to get wind and solar energy
+
+		//model solar power generated
+		double solar = double(0);
+		
+		//model wind power generated
+		double wind = double(0);
+		
+		//double production  = solar + wind 
+		double production = double(0);
+		
+		//model demand
+		double demand = double(0);
+		
+		//power city
+		boolean powered = false
+		double net = production - demand
+			if(net < 0 && stored > net)
+				storage - net;
+				powered = true;
+			else()
+
+
+		//switch case for storage
+			//case demand < solar + wind
+			//case demand = solar + wind
+			//case demand > solar + wind
+
 	}
 }
