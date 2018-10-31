b24a87425d7f99c9f12111547e125aaa793ac6af
CJ Coffey <40609439+cjCoffey@users.noreply.github.com>
2018-10-31 18:41:48 -0400

    Made things run
    more setup

--- CleanEnergyModel.java
+++ CleanEnergyModel.java
@@ -2,36 +2,44 @@ public class CleanEnergyModel
 {
 	public static void main(String[] args)
 	{
-		//args = [name] use this line to keep track of args needed
+		//args = [storage_capacity] use this line to keep track of args needed
+		final float STORAGE = Float.valueOf(args[0]);
+		float stored = 0.f; //init storage to 0 
 
-		System.out.print("Hello " + args[0] + "!");
+		System.out.println("Storage cap: " + STORAGE);
 		//parse weather file to get wind and solar energy
 
 		//model solar power generated
-		double solar = double(0);
+		float solar = 0.f;
 		
 		//model wind power generated
-		double wind = double(0);
+		float wind = 0.f;
 		
 		//double production  = solar + wind 
-		double production = double(0);
+		float production = 0.f;
 		
 		//model demand
-		double demand = double(0);
+		float demand = 0.f;
 		
 		//power city
-		boolean powered = false
-		double net = production - demand
-			if(net < 0 && stored > net)
-				storage - net;
-				powered = true;
-			else()
+		boolean powered = false;
+		float net = production - demand;
+		if(net < 0 && stored > net)
+		{
+			stored  = stored - net;
+			powered = true;
+		}
+		else
+		{
+			//TODO
+		}
 
 
 		//switch case for storage
 			//case demand < solar + wind
 			//case demand = solar + wind
 			//case demand > solar + wind
+		System.out.print("RAN");
 
 	}
 }
